6b6698a9d7491f9e8d5cbe66db1c3b7830861bd7
Worthen <dworthen@wsu.edu>
2019-03-14 09:31:19 -0700

    event delegation

---
+++ src/BlazorStatehaschangedDemo/EventDelegation/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStatehaschangedDemo.EventDelegation.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
